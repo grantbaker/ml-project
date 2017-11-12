88a5ebecc7724c38b184e8213b9665f9bd374916
matt <jema5033@colorado.edu>
2017-11-11 22:25:41 -0700

    grant is pretty cool i guess

--- HOC.py
+++ HOC.py
@@ -13,12 +13,12 @@ CSVF='data/MovieGenre (copy).csv'
 def getMovies(csvfile):
     with open(csvfile) as f:
             csv_reader = csv.reader(f, delimiter=',')
-            print(csv_reader)
+            (csv_reader)
             a=[]
             b=[]
             for row in csv_reader:
                 a.append(row[0])
-                b.append(row[4])
+                b.append(row[4].split('|'))
     return a,b
 
 
@@ -35,7 +35,7 @@ def HOC(path):
             for i in range(32):
                 for j in range(8):
                     hoc[n,i]+=hist[8*i+j]
-            hoc[n,:]=hoc[n,:]/sum(hoc[n,:])
+            #hoc[n,:]=hoc[n,:]/sum(hoc[n,:])
             n=n+1
         return hoc
 
@@ -43,35 +43,59 @@ class HOCmovie():
     def __init__(self,id,gen):
         self.id=id
         self.gen=gen
+        self.catvec=[]
         self.hoc=HOC(DATA_LOCATION+'/'+id+'.jpg')
         self.flhoc=self.hoc.flatten()
 
-mc = movie.MovieContainer()
-IDs,genres=getMovies(CSVF)
-hocs=[]
-gens=[]
-for i in range(1,len(IDs)):
-    gen=genres[i]
-    id=IDs[i]
-    try:
-        a=HOCmovie(id,gen)
-        c=1
-    except Exception as e:
-        c=-1
-        print(-1)
-    if c==1:
-        hocs.append(a.flhoc)
-        gens.append(a.gen)
-print(np.shape(hocs))
+class HOCmovieList():
+    def __init__(self,path):
+        IDs,genres=getMovies(path)
+        self.IDs=[]
+        self.movies = dict()
+        for i in range(1,len(IDs)):
+            gen=genres[i]
+            id=IDs[i]
+            try:
+                a=HOCmovie(id,gen)
+                c=1
+            except Exception as e:
+                c=-1
+                print(-1)
+            if c==1:
+                self.movies[id]=a
+                self.IDs.append(id)
+            catlist = []
+            for key in self.IDs:
+                for cat in self.movies[key].gen:
+                    if cat not in catlist:
+                        catlist.append(cat)
+        catlist.sort()
+        self.cats=catlist
+        for key in self.IDs:
+            self.movies[key].catvec = np.zeros(len(catlist))
+            for cat in self.movies[key].gen:
+                self.movies[key].catvec[catlist.index(cat)] = 1
 
+    def GenData(self,test_proportion=0.3):
+        key_list=self.IDs
+        split_i = int(len(key_list)*test_proportion)
+        test_list = key_list[0:split_i]
+        train_list = key_list[split_i:]
+        movies=self.movies
+        self.x_test = np.zeros((len(test_list), len(movies[test_list[0]].flhoc)), dtype=np.int8)
+        self.y_test = np.zeros((len(test_list), self.movies[test_list[0]].catvec.shape[0]), dtype=np.int8)
+        self.x_train = np.zeros((len(train_list), len(movies[train_list[0]].flhoc)), dtype=np.int8)
+        self.y_train = np.zeros((len(train_list), self.movies[train_list[0]].catvec.shape[0]), dtype=np.int8)
+        i=0
+        for j in test_list:
+            self.x_test[i]=(self.movies[j].flhoc)
+            self.y_test[i]=(self.movies[j].catvec)
+            i+=1
+        i=0
+        for j in train_list:
+            self.x_train[i]=(self.movies[j].flhoc)
+            self.y_train[i]=(self.movies[j].catvec)
+            i+=1
 
-catlist = []
-for key in self.movies.keys():
-    for cat in self.movies[key].genres:
-        if cat not in catlist:
-            catlist.append(cat)
-catlist.sort()
-for key in self.movies.keys():
-    self.movies[key].catvec = np.zeros(len(catlist))
-        for cat in self.movies[key].genres:
-        self.movies[key].catvec[catlist.index(cat)] = 1
+lis=HOCmovieList(CSVF)
+lis.GenData()
